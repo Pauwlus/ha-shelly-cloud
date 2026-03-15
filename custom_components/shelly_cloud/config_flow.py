1504aef09e715c1fb0f43d5c9ef936106d41a5e6
Pauwlus <42134561+Pauwlus@users.noreply.github.com>
2026-03-15 10:08:11 +0100

    Update config_flow.py

--- custom_components/shelly_cloud/config_flow.py
+++ custom_components/shelly_cloud/config_flow.py
@@ -1,16 +1,16 @@
-
 import voluptuous as vol
 from homeassistant import config_entries
 from .const import DOMAIN
 
-class ShellyCloudConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
+class MultiShellyCloudConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
     VERSION = 1
 
     async def async_step_user(self, user_input=None):
         if user_input is not None:
-            return self.async_create_entry(title="Shelly Cloud", data=user_input)
+            return self.async_create_entry(title=user_input["name"], data=user_input)
         schema = vol.Schema({
-            vol.Required("username"): str,
-            vol.Required("password"): str,
+            vol.Required("name"): str,
+            vol.Required("host"): str,
+            vol.Required("auth_key"): str
         })
         return self.async_show_form(step_id="user", data_schema=schema)
